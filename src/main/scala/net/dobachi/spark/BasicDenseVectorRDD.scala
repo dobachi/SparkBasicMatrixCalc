db3e27993f8ca94cdc6e0be0a8fb19891d453b2f
dobachi <dobachi@example.com>
2015-03-02 02:16:45 +0900

    Add BDV2Array helper implicit method to BasicDenseVectorRDD object

--- src/main/scala/net/dobachi/spark/BasicDenseVectorRDD.scala
+++ src/main/scala/net/dobachi/spark/BasicDenseVectorRDD.scala
@@ -5,10 +5,15 @@ import org.apache.spark.SparkContext._
 import breeze.linalg.{DenseVector => BDV}
 
 /**
- * The helper class for calculation of two matrices.
+ * The helper class for calculation of two RDD which have matrix-like data.
+ *
+ * @param rdd RDD of Breeze DenseVector which has values of Double type.
+ *
+ * @note You can use BasicDenseVectorRDD object's implicit method
+ *       to convert RDD[BDV[Double]] to RDD[Array[Double]] each other.
  */
 @SerialVersionUID(1L)
-class BasicDenseMatrix(rdd: RDD[BDV[Double]]) extends Serializable {
+class BasicDenseVectorRDD(rdd: RDD[BDV[Double]]) extends Serializable {
   lazy val rddWithIndex: RDD[(Long, BDV[Double])] = rdd.zipWithIndex().map(p => (p._2, p._1))
 
   private def join(other: RDD[BDV[Double]]): RDD[(BDV[Double], BDV[Double])] = {
@@ -37,8 +42,15 @@ class BasicDenseMatrix(rdd: RDD[BDV[Double]]) extends Serializable {
   }
 }
 
-object BasicMatrix {
+/**
+ * The helper for BasicDenseVectorRDD class
+ */
+object BasicDenseVectorRDD {
   implicit def array2BDV(rdd: RDD[Array[Double]]): RDD[BDV[Double]] = {
     rdd.map(p => BDV(p))
+  }
+  
+  implicit def BDV2Array(rdd: RDD[BDV[Double]]): RDD[Array[Double]] = {
+    rdd.map(p => p.toArray)
   }
 }
