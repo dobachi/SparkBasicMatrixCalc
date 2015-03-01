279eaf5ec04456d37e5ef9ed9cae31ae1f0cf315
dobachi <dobachi@example.com>
2015-03-02 01:01:47 +0900

    Add ClassTag

--- src/main/scala/net/dobachi/spark/BasicMatrix.scala
+++ src/main/scala/net/dobachi/spark/BasicMatrix.scala
@@ -10,30 +10,30 @@ import scala.reflect.ClassTag
  * The helper class for calculation of two matrices.
  */
 @SerialVersionUID(1L)
-class BasicMatrix(rdd: RDD[BV]) extends Serializable {
+class BasicMatrix[T: ClassTag](rdd: RDD[BV[T]]) extends Serializable {
   lazy val rddWithIndex: RDD[(Long, BV)] = rdd.zipWithIndex().map(p => (p._2, p._1))
 
-  private def join(other: RDD[BV]): RDD[(BV, BV)] = {
+  private def join[T: ClassTag](other: RDD[BV[T]]): RDD[(BV[T], BV[T])] = {
     lazy val otherWithIndex: RDD[(Long, BV)] = other.zipWithIndex().map(p => (p._2, p._1))
     rddWithIndex.join(otherWithIndex).map(p => (p._2._1, p._2._2))
   }
 
-  def :+(other: RDD[BV]): RDD[BV] = {
+  def :+[T: ClassTag](other: RDD[BV[T]]): RDD[BV[T]] = {
     val joined = join(other)
     joined.map(p => p._1 :+ p._2)
   }
 
-  def :-(other: RDD[BV]): RDD[BV] = {
+  def :-[T: ClassTag](other: RDD[BV[T]]): RDD[BV[T]] = {
     val joined = join(other)
     joined.map(p => p._1 :- p._2)
   }
 
-  def :/(other: RDD[BV]): RDD[BV] = {
+  def :/[T: ClassTag](other: RDD[BV[T]]): RDD[BV[T]] = {
     val joined = join(other)
     joined.map(p => p._1 :/ p._2)
   }
 
-  def :*(other: RDD[BV]): RDD[BV] = {
+  def :*[T: ClassTag](other: RDD[BV[T]]): RDD[BV[T]] = {
     val joined = join(other)
     joined.map(p => p._1 :* p._2)
   }
